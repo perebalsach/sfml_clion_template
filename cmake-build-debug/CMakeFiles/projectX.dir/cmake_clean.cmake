0b779ad9bd65fb147422224d06ecc04680ec703b
Joshix <joshix.dev@mailbox.org>
2021-04-02 13:47:47 +0200

    improve project

---
+++ cmake-build-debug/CMakeFiles/projectX.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projectX.dir/main.cpp.o"
+  "projectX"
+  "projectX.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projectX.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,22 +1,23 @@
 #include <SFML/Graphics.hpp>
 
+using namespace sf;
 
 int main() {
 
-    sf::RenderWindow window(sf::VideoMode(640,480,32),"Hello SFML");
+    RenderWindow window(VideoMode(640,480,32),"Hello SFML");
 
-    sf::Font font;
-    font.loadFromFile("resources/fonts/Pacifico.ttf");
+    Font font;
+    font.loadFromFile("../resources/fonts/Pacifico.ttf");
 
-    sf::Text text("Hello World",font,11);
+    Text text("Hello World",font,11);
     text.setCharacterSize(32);
-    text.setPosition(window.getSize().x/2 - text.getGlobalBounds().width/2,
-                     window.getSize().y/2 - text.getGlobalBounds().height/2);
+    text.setPosition(window.getSize().x/2.0f - text.getGlobalBounds().width/2.0f,
+                     window.getSize().y/2.0f - text.getGlobalBounds().height/2.0f);
 
 
     while(window.isOpen()){
 
-        sf::Event event;
+        Event event{};
         while(window.pollEvent(event)) {
             if(event.type == sf::Event::Closed){
                 window.close();
