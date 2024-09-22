91c91151036d353db1773a94db1dc236a071e7c6
Pere Balsach <pere.balsach@gmail.com>
2024-09-22 21:35:08 +0200

    Updated the template with a more generic approach and better solution

---
+++ README.txt
@@ -0,0 +1,16 @@
+# CMake SFML Template 
+
+SFML template project to start developing any app with the C++ framework.
+Tested on windows as for now.
+
+### Windows users
+You need to copy the dll's to the build folder
+* openal32.dll
+* sfml-audio-2.dll
+* sfml-audio-d-2.dll
+* sfml-graphics-2.dll
+* sfml-graphics-d-2.dll
+* sfml-system-2.dll
+* sfml-system-d-2.dll
+* sfml-window-2.dll
+* sfml-window-d-2.dll
--- main.cpp
+++ main.cpp
@@ -1,31 +1,27 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
+int main(int argc, char* argv[]) {
 
-int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    sf::Clock clock;
 
-    sf::RenderWindow window(sf::VideoMode(640,480,32),"Hello SFML");
+    while (window.isOpen()) {
+        sf::Event event{};
 
-    sf::Font font;
-    font.loadFromFile("resources/fonts/Pacifico.ttf");
-
-    sf::Text text("Hello World",font,11);
-    text.setCharacterSize(32);
-    text.setPosition(window.getSize().x/2 - text.getGlobalBounds().width/2,
-                     window.getSize().y/2 - text.getGlobalBounds().height/2);
-
-
-    while(window.isOpen()){
-
-        sf::Event event;
-        while(window.pollEvent(event)) {
-            if(event.type == sf::Event::Closed){
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
                 window.close();
-            }
-
-            window.clear(sf::Color::Black);
-            window.draw(text);
-            window.display();
         }
+
+        if (clock.getElapsedTime().asSeconds() >= 50) {
+            window.close();
+        }
+
+        window.clear(sf::Color::Black);
+        
+        window.display();
     }
+
     return 0;
 }
